33d1a5331b374b81972445beebff8029db0f110f
Durohombredeez7 <Durohombredeez7@gmail.com>
2019-12-06 16:04:40 -0800

    brief update

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var plus = i;
+var minus = j;
+
+
+for (let i = 0; i < 200;i++) {
+    function eventHandler(onClick) {
+        if (button.clicked == 'addition') {
+          /* handle a full screen toggle */
+        } else /* fullscreenerror */ {
+          /* handle a full screen toggle error */
+        }
+      }
+
+}
+
+
+
+
+
+
