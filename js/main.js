bb9a4abe6a8bbfb4d13a9ffee0dd12d8724c3c4c
Durohombredeez7 <Durohombredeez7@gmail.com>
2019-12-06 16:24:40 -0800

    another update

--- js/main.js
+++ js/main.js
@@ -3,18 +3,17 @@ var minus = j;
 
 
 for (let i = 0; i < 200;i++) {
-    function eventHandler(onClick) {
-        if (button.clicked == 'addition') {
-          /* handle a full screen toggle */
-        } else /* fullscreenerror */ {
-          /* handle a full screen toggle error */
-        }
-      }
+    // find a way to use this to implement addition
+    // and use j-- for subtraction
+    // try to apply this function to my "result" output
 
 }
 
-
-
-
+function eventHandler(onClick) {
+        if (Object.onclick == 'plus') {
+          console.log("adding");
+        } else (Object.onclick == 'minus') {
+          console.log("subtracting");
+        }}
 
 
